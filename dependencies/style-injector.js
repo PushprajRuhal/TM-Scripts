331ea9d8a09eb7b6cc775bc3f207b7d62734cb91
Pushpraj Ruhal <pur@sitecore.net>
2017-10-26 12:32:30 +0800

    Fixed #1, rules can now be marked !important.

--- dependencies/style-injector.js
+++ dependencies/style-injector.js
@@ -1,13 +1,18 @@
     //style injector
-    var getStyleInjector = function() {
+    var getStyleInjector = function(markImportant) {
         var stylesheet = null;
+        var important = /((?:^|;)[\w-:\s\(\),\.%]+?(?!important))(?=;|$)/gi;
+        var shouldMarkImportant = markImportant | false;
         return {
-            insertRule:function(selector,rules)
+            insertRule:function(selector, rules, markImportant)
             {
                 if(!this.stylesheet){
                     var context = document,stylesheet;
                     context.getElementsByTagName('head')[0].appendChild(context.createElement('style'));
                     this.stylesheet=context.styleSheets[context.styleSheets.length-1];
+                }
+                if(markImportant | shouldMarkImportant){
+                    rules = rules.replace(important,"$1 !important");
                 }
                 for(var i=0;i<selector.length;++i){
                     this.stylesheet.addRule(selector[i], rules);
